008d3ca192892d19398f73bfaad7d05e8eb492b6
ZhaoQian <zhaoqianjava@gmail.com>
2014-04-01 23:57:28 +0800

    test

---
+++ credo-test/src/main/java/org/credo/spring/a1/MyBean.java
@@ -0,0 +1,11 @@
+package org.credo.spring.a1;
+
+public class MyBean {
+    private String testStr = "Hello Spring";
+    public String getTestStr() {
+	return testStr;
+    }
+    public void setTestStr(String testStr) {
+	this.testStr = testStr;
+    }
+}
--- credo-test/src/main/java/org/credo/xdocreport/ODTProjectWithVelocity2PDF.java
+++ credo-test/src/main/java/org/credo/xdocreport/ODTProjectWithVelocity2PDF.java
@@ -12,7 +12,7 @@ import org.credo.xdocreport.model.Project;
 import org.odftoolkit.odfdom.converter.pdf.PdfOptions;
 
 import com.lowagie.text.DocumentException;
-import com.lowagie.text.Font;
+import com.lowagie.text.Font; 
 import com.lowagie.text.pdf.BaseFont;
 
 import fr.opensagres.xdocreport.converter.ConverterTypeTo;
@@ -27,50 +27,58 @@ import fr.opensagres.xdocreport.template.TemplateEngineKind;
 
 public class ODTProjectWithVelocity2PDF {
 
-//	public static void main(String[] args) throws DocumentException {
-//		try {
-//			// 1) Load ODT file by filling Velocity template engine and cache it
-//			// to the registry
-//			InputStream in = new FileInputStream(new File("E:\\temp\\ODTProjectWithVelocity.odt"));
-//			IXDocReport report = XDocReportRegistry.getRegistry().loadReport(in, TemplateEngineKind.Velocity);
-//
-//			// 2) Create context Java model
-//			IContext context = report.createContext();
-//			Project project = new Project("天气");
-//			context.put("project", project);
-//
-//			// 3) Generate report by merging Java model with the ODT
-//			OutputStream out = new FileOutputStream(new File("E:\\temp\\ODTProjectWithVelocity_Out.pdf"));
-//			// ODFDOM-->ODT XWPF--->DOCX
-//			Options options = Options.getTo(ConverterTypeTo.PDF).via(ConverterTypeVia.ODFDOM).subOptions(
-//					PdfOptions.create().fontProvider(new IFontProvider() {
-//
-//				public Font getFont(String familyName, String encoding, float size, int style, Color color) {
-//					System.out.println("familyName:"+familyName);
-//					System.out.println("encoding:"+encoding);
-//					System.out.println("size:"+size);
-//					System.out.println("style:"+style);
-//					System.out.println("color:"+color);
-//					try {
-//						// STSong-Light C:/Windows/Fonts/arialuni.ttf   UniGB-UCS2-H BaseFont.IDENTITY_H
-//						BaseFont bfChinese = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H", BaseFont.EMBEDDED);
-//						Font fontChinese = new Font(bfChinese, size, style, color);
-//						if (familyName != null)
-//							fontChinese.setFamily(familyName);
-//						return fontChinese;
-//					} catch (Throwable e) {
-//						e.printStackTrace();
-//						//return ITextFontRegistry.getRegistry().getFont(familyName, encoding, size, style, color);
-//					}
-//				}
-//			}));
-//
-//			report.convert(context, options, out);
-//			System.out.println("Over");
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		} catch (XDocReportException e) {
-//			e.printStackTrace();
-//		}
-//	}
+    // public static void main(String[] args) throws DocumentException {
+    // try {
+    // // 1) Load ODT file by filling Velocity template engine and cache it
+    // // to the registry
+    // InputStream in = new FileInputStream(new
+    // File("E:\\temp\\ODTProjectWithVelocity.odt"));
+    // IXDocReport report = XDocReportRegistry.getRegistry().loadReport(in,
+    // TemplateEngineKind.Velocity);
+    //
+    // // 2) Create context Java model
+    // IContext context = report.createContext();
+    // Project project = new Project("天气");
+    // context.put("project", project);
+    //
+    // // 3) Generate report by merging Java model with the ODT
+    // OutputStream out = new FileOutputStream(new
+    // File("E:\\temp\\ODTProjectWithVelocity_Out.pdf"));
+    // // ODFDOM-->ODT XWPF--->DOCX
+    // Options options =
+    // Options.getTo(ConverterTypeTo.PDF).via(ConverterTypeVia.ODFDOM).subOptions(
+    // PdfOptions.create().fontProvider(new IFontProvider() {
+    //
+    // public Font getFont(String familyName, String encoding, float size, int
+    // style, Color color) {
+    // System.out.println("familyName:"+familyName);
+    // System.out.println("encoding:"+encoding);
+    // System.out.println("size:"+size);
+    // System.out.println("style:"+style);
+    // System.out.println("color:"+color);
+    // try {
+    // // STSong-Light C:/Windows/Fonts/arialuni.ttf UniGB-UCS2-H
+    // BaseFont.IDENTITY_H
+    // BaseFont bfChinese = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H",
+    // BaseFont.EMBEDDED);
+    // Font fontChinese = new Font(bfChinese, size, style, color);
+    // if (familyName != null)
+    // fontChinese.setFamily(familyName);
+    // return fontChinese;
+    // } catch (Throwable e) {
+    // e.printStackTrace();
+    // //return ITextFontRegistry.getRegistry().getFont(familyName, encoding,
+    // size, style, color);
+    // }
+    // }
+    // }));
+    //
+    // report.convert(context, options, out);
+    // System.out.println("Over");
+    // } catch (IOException e) {
+    // e.printStackTrace();
+    // } catch (XDocReportException e) {
+    // e.printStackTrace();
+    // }
+    // }
 }
