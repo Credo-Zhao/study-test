2fe85089e109a6f2e7c7fc03be0f3c03488e5ead
ZhaoQian <zhaoqianjava@gmail.com>
2014-04-27 23:51:27 +0800

    提交

--- credo-test/src/main/java/org/credo/lang/BigDecimalTest.java
+++ credo-test/src/main/java/org/credo/lang/BigDecimalTest.java
@@ -4,13 +4,16 @@ import java.math.BigDecimal;
 public class BigDecimalTest {
 
 	public static void main(String[] args) {
-			BigDecimalTest aaa = null;
-			System.out.println("aaa:"+aaa);
-			//System.out.println("2:"+aaa.intValue()); java.lang.NullPointerException
-			
-			BigDecimal bbb=new BigDecimal(0);
-			System.out.println("bbb:"+bbb);
-			System.out.println("2:"+bbb.intValue());
+		double totalPrice = 1.12;
+		for(int i=0;i<6;i++){
+			totalPrice=totalPrice+1.13534;
+		}
+		BigDecimal b = new BigDecimal(Double.toString(totalPrice * 0.17));
+		BigDecimal one = new BigDecimal("1");
+		double varPrice = b.divide(one, 2, BigDecimal.ROUND_HALF_UP).doubleValue();
+		System.out.println(totalPrice);
+		System.out.println(varPrice);
+		System.out.println(totalPrice * 0.17);
 	}
 
 }
